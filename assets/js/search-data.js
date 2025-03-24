93c5328cc43f224cfdf6234bf594389f6a168dc6
nwp249 <petersonn249@gmail.com>
2025-03-24 00:02:43 +0000

    Deploying to gh-pages from @ nwp249/nwp249.github.io@3499188b5da3b270ba454d4e7426a9e6ddf7e30d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,12 +38,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
+      },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
